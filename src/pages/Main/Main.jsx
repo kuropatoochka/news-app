97864e033ac82b83112df3d7f6cc9ded1c62c6f5
valya000999@gmail.com <valya000999@gmail.com>
2025-01-23 14:25:15 +0300

    add pagination, change api

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -4,26 +4,32 @@ import NewsList from "../../components/NewsList/NewsList.jsx"
 import Banner from "../../components/Banner/Banner.jsx"
 import LastNewsList from "../../components/LastNewsList/LastNewsList.jsx"
 import CategoryList from "../../components/CategoryList/CategoryList.jsx"
+import Pagination from "../../components/Pagination/Pagination.jsx"
 import styles from './styles.module.css'
 
 const Main = () => {
   const [news, setNews] = useState([])
   const [categories, setCategories] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [currentPage, setCurrentPage] = useState(1)
+  const totalPages = 10
+  const pageSize = 15
+
+  const fetchNews = async (currentPage) => {
+    try {
+      setIsLoading(true)
+      const response = await getNews(currentPage, pageSize)
+      setNews(response.news)
+      setIsLoading(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   useEffect(() => {
-    const fetchNews = async () => {
-      try {
-        setIsLoading(true)
-        const response = await getNews()
-        setNews(response.news)
-        setIsLoading(false)
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    fetchNews()
-  }, [])
+    fetchNews(currentPage)
+  }, [currentPage])
+
   useEffect(() => {
     const fetchCategory = async () => {
       try {
@@ -35,13 +41,33 @@ const Main = () => {
     }
     fetchCategory()
   }, [])
+  
+  const handleNextPage = () => {
+    currentPage < totalPages ? setCurrentPage(currentPage + 1) : null
+  }
+  const handlePreviousPage = () => {
+    currentPage > 1 ? setCurrentPage(currentPage - 1) : null
+  }
+  const handlePageClick = (pageNumber) => {
+    setCurrentPage(pageNumber)
+  }
+
   return (
     <main className={styles.main}>
       <Banner news={news} isLoading={isLoading}/>
       <div className={styles.main__news}>
         <CategoryList categories={categories}/>
-        <NewsList news={news} isLoading={isLoading}/>
         <LastNewsList news={news}/>
+        <div>
+          <NewsList news={news} isLoading={isLoading}/>
+          <Pagination
+            handleNextPage={handleNextPage}
+            handlePreviousPage={handlePreviousPage}
+            handlePageClick={handlePageClick}
+            currentPage = {currentPage}
+            totalPages={totalPages}
+          />
+        </div>
       </div>
     </main>
   )
