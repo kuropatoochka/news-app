ff544e63d55f24d3b23e2a702dbc69017e3173e7
valya000999@gmail.com <valya000999@gmail.com>
2025-02-12 10:36:26 +0300

    add auto-scroll banner

--- src/components/LatestNewsList/LatestNewsList.jsx
+++ src/components/LatestNewsList/LatestNewsList.jsx
@@ -2,41 +2,50 @@ import darkArrow from '../../assets/chevron_dark.svg'
 import styles from './styles.module.css'
 import withSkeleton from "../../helpers/hocs/withSkeleton.jsx"
 import LatestNewsItem from "../LatestNewsItem/LatestNewsItem.jsx"
-import {useRef, useState} from "react"
+import {useEffect, useMemo, useRef, useState} from "react"
 
 const LatestNewsList = ({ news }) => {
-  const itemRef = useRef(null)
   const [offsetY, setOffsetY] = useState(0)
+  const itemRef = useRef(null)
+  const intervalRef = useRef(null)
 
-  const handleUpClick = () => {
+  const updateOffset = (direction) => {
+    const maxOffset = -(itemRef.current.offsetHeight * (news.length - 1))
     setOffsetY((currentOffset) => {
-      return Math.min(currentOffset + itemRef.current.offsetHeight, 0)
+      return (direction === "up")
+        ? Math.min(currentOffset + itemRef.current.offsetHeight, 0)
+        : (currentOffset !== maxOffset) ? currentOffset - itemRef.current.offsetHeight : 0
     })
+    resetInterval()
   }
 
-  const handleDownClick = () => {
-    setOffsetY((currentOffset) => {
-      const maxOffset = -(itemRef.current.offsetHeight * (news.length - 1))
-      return Math.max(currentOffset - itemRef.current.offsetHeight, maxOffset)
-    })
+  const resetInterval = () => {
+    if (intervalRef.current) clearInterval(intervalRef.current)
+    intervalRef.current = setInterval(() => updateOffset("down"), 5000)
   }
+
+  useEffect(() => {
+    resetInterval()
+    return () => clearInterval(intervalRef.current)
+  }, [])
+
+  const renderListOfNews = useMemo(() =>
+    news.map(item => <LatestNewsItem key={item.id} item={item} itemRef={itemRef}/>),
+    [news]
+  )
 
   return (
-    <div className={styles.container} ref={itemRef}>
-      <div className={styles.window}>
-        <div className={styles.list}
-             style={{transform: `translateY(${offsetY}px)`}}
-        >
-          {news.map(item => (
-            <LatestNewsItem key={item.id} item={item} itemRef={itemRef}/>
-          ))}
+    <>
+      <div className={styles.window} ref={itemRef}>
+        <div className={styles.list} style={{transform: `translateY(${offsetY}px)`}}>
+          {renderListOfNews}
         </div>
       </div>
       <div className={styles.toggle_buttons}>
-        <img className={styles.arrowUp} src={darkArrow} alt="arrowUp" onClick={handleUpClick}/>
-        <img className={styles.arrowDown} src={darkArrow} alt="arrowDown" onClick={handleDownClick}/>
+        <img className={styles.arrowUp} src={darkArrow} alt="arrowUp" onClick={() => updateOffset("up")}/>
+        <img className={styles.arrowDown} src={darkArrow} alt="arrowDown" onClick={() => updateOffset("down")}/>
       </div>
-    </div>
+    </>
   )
 }
 
